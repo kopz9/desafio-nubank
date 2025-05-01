f59510e205ee7639ff85f38f4ed5c2603395520b
kopz9 <gustavoeduardo438@gmail.com>
2025-05-01 12:57:05 -0300

    feat: cliente repository

---
+++ src/main/java/me/kopz/nubank/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package me.kopz.nubank.repository;
+
+import me.kopz.nubank.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
